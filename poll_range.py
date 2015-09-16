80f1e2c5dd8e06791c068a3234fce72f43282c8b
Sebastian Mai <bastinat0r@bastinat0r.de>
2015-09-16 15:39:50 +0000

    Fixing continuos polling

--- poll_range.py
+++ poll_range.py
@@ -1,12 +1,16 @@
+#!/usr/bin/python2.7
 from i2cranging import *
 import argparse
 
+def auto_int(i):
+    return int(i, 0)
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='continuously read range from i2c ranging node')
-    parser.add_argument('address', help='i2c address of ranging node to be used')
-    parser.add_argument('time', help='time to wait between each ranging')
+    parser.add_argument('address', type=auto_int, help='i2c address of ranging node to be used')
+    parser.add_argument('--time', type=float, default=1, help='time to wait between each ranging')
     args = parser.parse_args()
     while True:
         start_ranging(args.address)
-        read_ranging_result(args.address)
+        print read_ranging_result(args.address)
         time.sleep(args.time)
