0fc15992f52161a847cfd4c5f7b6fa01bad781e6
Sebastian Mai <bastinat0r@bastinat0r.de>
2015-09-28 11:45:59 +0200

    add csv-formatted output

--- poll_range.py
+++ poll_range.py
@@ -9,10 +9,15 @@ queue = []
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='continuously read range from i2c ranging node')
-    parser.add_argument('address', type=auto_int, help='i2c address of ranging node to be used')
+    parser.add_argument('address', type=auto_int, help='i2c address of ranging node to be used', required=True)
     parser.add_argument('--time', type=float, default=1, help='time to wait between each ranging')
     parser.add_argument('--target', type=auto_int, help='ranging id of target node')
+    parser.add_argument('--num', type=auto_int, help='number of readings to take')
+    parser.add_argument('--csv', dest='csv', action='store_true')
+    parser.set_defaults(feature=False)
     args = parser.parse_args()
+    if args.csv:
+        print "from, to, dqf, range"
     while True:
         set_reflector_addr(args.address, args.target)
         try:
@@ -26,8 +31,11 @@ if __name__ == '__main__':
             for i in queue:
                 sum += i[0]
                 div += i[1]
-            if(div > 0):
-                print "0x%x -> 0x%x, dqf: %i, range: %i, avg: %i" %(range_result['addr1'],range_result['addr2'],range_result['dqf'], range_result['range'], sum / len(queue))
-        except IOError:
-            print "i2c Error"
+            if args.csv:
+                "%x, %x, %i, %i" %(range_result['addr1'],range_result['addr2'],range_result['dqf'], range_result['range'])
+            else:
+                if(div > 0):
+                    print "0x%x -> 0x%x, dqf: %i, range: %i, avg: %i" %(range_result['addr1'],range_result['addr2'],range_result['dqf'], range_result['range'], sum / len(queue))
+            except IOError:
+                print "i2c Error"
         time.sleep(args.time)
